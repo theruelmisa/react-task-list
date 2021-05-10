0cf24781096b1d6365e11bb26afff3180636c015
Rumies <rumies.media@gmail.com>
2021-05-10 11:35:31 -0400

    finished overall styling of app

--- src/components/TaskList/Task/Task.jsx
+++ src/components/TaskList/Task/Task.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
+import styles from './Task.module.css'; 
 
 const Task = ({ task, onRemoveTask }) => {
+    const [ done, setDone ] = useState(false);
+
+    const onToggleDone = () => {
+        setDone(!done);
+    }
+
     return (
-        <li onClick={ () => onRemoveTask( task.id )}>
-            { task.name }
+        <li className={ styles.Task } title="Click to mark as done" onClick={ onToggleDone }>
+            <p className={ `${ done && styles.Done }` }>{ task.name }</p>
+            <div className={ styles.Delete } onClick={ () => onRemoveTask( task.id )} title="Click to remove">Delete</div>
         </li>
     )
 }
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Task from './Task/Task';
+import styles from './TaskList.module.css';
 
 const TaskList = ({ tasks, removeTaskHandler }) => {
     return (
-        <>
-            <h1> Tasks </h1>
+        <div className={ styles.Container }>
+            <h1 className={ styles.Title }> Tasks </h1>
             <ul>
                 {
                     tasks && tasks.map( task => ( <Task key={ task.id } task={ task } onRemoveTask={ removeTaskHandler } /> ))
                 }
             </ul>
-        </>
+        </div>
     )
 };
 
